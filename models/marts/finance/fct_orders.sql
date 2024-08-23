0d6ec6c3ce3b89c61411b1a13efc3aaac05527b1
Adrian Maroto <amarotohuertas@gmail.com>
2024-08-23 14:02:37 +0000

    Complete first part of the refactoring sql course

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -10,7 +10,7 @@ with
     order_payments as (
         select
             order_id,
-            sum(case when status = 'success' then amount end) as amount
+            sum(case when payment_status = 'success' then payment_amount end) as amount
         from payments
         group by 1
     ),
